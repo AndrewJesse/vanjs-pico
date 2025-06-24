3c6e18e48fbc2d8fe1252e9f24a63923bd3d188d
AJ <aj.marchese@outlook.com>
2025-06-23 18:57:05 -0700

    establish table in main

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
+import van from 'vanjs-core'
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const {table, tr, th, div, td} = van.tags
 
-setupCounter(document.querySelector('#counter'))
+
+export const Table = () => { 
+    return div(
+    table(),
+        tr(),
+            th("Name"),
+            th("Price"),
+            th("Image"),
+        tr(),
+            td("Hammer"),
+            td("$100"),
+            td("[picture]"),
+        tr(),
+            td("Drill"),
+            td("$200"),
+            td("[picture]"),
+    )
+}
+
+van.add(document.body, Table())
